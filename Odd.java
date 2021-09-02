65ed9d1b728e3f75d914dc7cb2d1c4ab61c0015f
AISHWARYA MEHTA <59408091+aishwarya-mehta@users.noreply.github.com>
2021-09-02 21:55:47 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,24 @@
+package com.thoughtpearl;
+
+public class Odd {
+	
+	public static void main(String []args)
+	{
+		int n = 3;
+		if(n == 1)
+		{
+			System.out.println(n + " is not odd number");
+		}
+		else
+		{
+			if(n%2 != 0)
+			{
+				System.out.println(n + " is odd number");
+			}
+			else
+			{
+			System.out.println(n+ " is not odd number");
+			}
+		}
+	}
+}
